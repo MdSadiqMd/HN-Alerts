12ab5385a0bd8d2e379074579afe5ffbf062de80
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-11-30 02:46:26 +0530

    kv store segregation

---
+++ internal/kv-store.go
@@ -0,0 +1,79 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/syumai/workers/cloudflare/kv"
+)
+
+const hnAlertsNamespace = "HN_ALERTS"
+const hnAlertsKey = "hn-alerts"
+
+func GetHNTop10FromKV(req *http.Request, titles []string) ([]string, error) {
+	HNAlertsKV, err := kv.NewNamespace(hnAlertsNamespace)
+	if err != nil {
+		fmt.Println("KV namespace error:", err)
+		return nil, err
+	}
+
+	var oldTitles []string
+	fmt.Println("Reading old titles from KV cache...")
+	for i := 0; i < 10; i++ {
+		key := fmt.Sprintf("%d", i)
+		title, err := HNAlertsKV.GetString(key, nil)
+		if err != nil {
+			break
+		}
+		if title != "" {
+			oldTitles = append(oldTitles, title)
+		}
+	}
+	fmt.Printf("Found %d existing titles in KV cache\n", len(oldTitles))
+
+	oldTitlesSet := make(map[string]bool)
+	for _, title := range oldTitles {
+		oldTitlesSet[title] = true
+	}
+
+	var uniqueTop10 []string
+	for _, title := range titles {
+		if title == "" {
+			continue
+		}
+		if !oldTitlesSet[title] && len(uniqueTop10) < 10 {
+			uniqueTop10 = append(uniqueTop10, title)
+		}
+	}
+	fmt.Printf("Selected %d unique titles (ignoring duplicates)\n", len(uniqueTop10))
+
+	fmt.Println("Unique Top 10 Titles:")
+	for i, title := range uniqueTop10 {
+		fmt.Printf("%d. %s\n", i+1, title)
+	}
+
+	fmt.Println("Deleting old titles from KV cache...")
+	for i := 0; i < len(oldTitles); i++ {
+		key := fmt.Sprintf("%d", i)
+		err := HNAlertsKV.Delete(key)
+		if err != nil {
+			fmt.Printf("KV Delete error for key=%s: %v\n", key, err)
+		}
+	}
+
+	fmt.Printf("Storing %d new titles in KV cache...\n", len(uniqueTop10))
+	for i, title := range uniqueTop10 {
+		key := fmt.Sprintf("%d", i)
+		err = HNAlertsKV.PutString(key, title, nil)
+		if err != nil {
+			fmt.Printf("KV PutString error for key=%s: %v\n", key, err)
+			return nil, err
+		}
+	}
+
+	response := fmt.Sprintf("Top %d Unique HN Feeds:\n", len(uniqueTop10))
+	for i, title := range uniqueTop10 {
+		response += fmt.Sprintf("%d. %s\n", i+1, title)
+	}
+	return uniqueTop10, nil
+}
--- main.go
+++ main.go
@@ -8,11 +8,7 @@ import (
 
 	"github.com/MdSadiqMd/HN-Alerts/internal"
 	"github.com/syumai/workers"
-	"github.com/syumai/workers/cloudflare/kv"
 )
-
-const hnAlertsNamespace = "HN_ALERTS"
-const hnAlertsKey = "hn-alerts"
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
@@ -36,73 +32,17 @@ func main() {
 		}
 		fmt.Printf("Fetched %d titles from HN\n", len(top20Titles))
 
-		HNAlertsKV, err := kv.NewNamespace(hnAlertsNamespace)
+		uniqueTop10, err := internal.GetHNTop10FromKV(req, top20Titles)
 		if err != nil {
-			fmt.Println("KV namespace error:", err)
+			fmt.Println("Error getting top 10 from KV:", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		var oldTitles []string
-		fmt.Println("Reading old titles from KV cache...")
-		for i := 0; i < 10; i++ {
-			key := fmt.Sprintf("%d", i)
-			title, err := HNAlertsKV.GetString(key, nil)
-			if err != nil {
-				break
-			}
-			if title != "" {
-				oldTitles = append(oldTitles, title)
-			}
-		}
-		fmt.Printf("Found %d existing titles in KV cache\n", len(oldTitles))
-
-		oldTitlesSet := make(map[string]bool)
-		for _, title := range oldTitles {
-			oldTitlesSet[title] = true
-		}
-
-		var uniqueTop10 []string
-		for _, title := range top20Titles {
-			if title == "" {
-				continue
-			}
-			if !oldTitlesSet[title] && len(uniqueTop10) < 10 {
-				uniqueTop10 = append(uniqueTop10, title)
-			}
-		}
-		fmt.Printf("Selected %d unique titles (ignoring duplicates)\n", len(uniqueTop10))
-
-		fmt.Println("Unique Top 10 Titles:")
+		w.Write([]byte(fmt.Sprintf("Top %d Unique HN Feeds:\n", len(uniqueTop10))))
 		for i, title := range uniqueTop10 {
-			fmt.Printf("%d. %s\n", i+1, title)
+			w.Write([]byte(fmt.Sprintf("%d. %s\n", i+1, title)))
 		}
-
-		fmt.Println("Deleting old titles from KV cache...")
-		for i := 0; i < len(oldTitles); i++ {
-			key := fmt.Sprintf("%d", i)
-			err := HNAlertsKV.Delete(key)
-			if err != nil {
-				fmt.Printf("KV Delete error for key=%s: %v\n", key, err)
-			}
-		}
-
-		fmt.Printf("Storing %d new titles in KV cache...\n", len(uniqueTop10))
-		for i, title := range uniqueTop10 {
-			key := fmt.Sprintf("%d", i)
-			err = HNAlertsKV.PutString(key, title, nil)
-			if err != nil {
-				fmt.Printf("KV PutString error for key=%s: %v\n", key, err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-		}
-
-		response := fmt.Sprintf("Top %d Unique HN Feeds:\n", len(uniqueTop10))
-		for i, title := range uniqueTop10 {
-			response += fmt.Sprintf("%d. %s\n", i+1, title)
-		}
-		w.Write([]byte(response))
 	})
 	workers.Serve(nil) // use http.DefaultServeMux
 }
