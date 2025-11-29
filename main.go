28787fb77fda041c610a975c9fafbc1e408cabbf
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-11-29 23:20:49 +0530

    HN API test, it works

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/syumai/workers"
+	"github.com/syumai/workers/cloudflare/fetch"
 )
 
 func main() {
@@ -20,6 +22,26 @@ func main() {
 		}
 		io.Copy(w, bytes.NewReader(b))
 	})
+	http.HandleFunc("/hn-alerts", func(w http.ResponseWriter, req *http.Request) {
+		cli := fetch.NewClient()
+
+		r, err := fetch.NewRequest(req.Context(), http.MethodGet, "https://hacker-news.firebaseio.com//v0/topstories.json?print=pretty", nil)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		r.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/111.0")
+
+		res, err := cli.Do(r, nil)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("content-type", "application/json")
+		io.Copy(w, res.Body)
+	})
 	workers.Serve(nil) // use http.DefaultServeMux
 }
-
