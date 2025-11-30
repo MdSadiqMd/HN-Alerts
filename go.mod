68c18be0cba8119b88a49a76ac0c77ab42136601
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-11-30 22:26:27 +0530

    final touch done

--- go.mod
+++ go.mod
@@ -3,5 +3,3 @@ module github.com/MdSadiqMd/HN-Alerts
 go 1.24.10
 
 require github.com/syumai/workers v0.31.0
-
-require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -6,10 +6,9 @@ import (
 	"io"
 	"net/http"
 
-	// "os"
-
 	"github.com/MdSadiqMd/HN-Alerts/internal"
 	"github.com/syumai/workers"
+	"github.com/syumai/workers/cloudflare"
 )
 
 func main() {
@@ -26,15 +25,6 @@ func main() {
 	})
 
 	http.HandleFunc("/hn-alerts", func(w http.ResponseWriter, req *http.Request) {
-		// godotenv.Load(".env")
-		// botApiString := os.Getenv("BOT_GREEN_API")
-		// if botApiString == "" {
-		// 	log.Fatal("Bot Green Api is not Found")
-		// 	return
-		// }
-
-		fmt.Println("Got BOT API! and Fetching top 20 stories from HN...")
-
 		top20Stories, err := internal.FetchHNTopN(req, 20)
 		if err != nil {
 			fmt.Println("Error fetching from HN:", err)
@@ -50,23 +40,16 @@ func main() {
 			return
 		}
 
-		respStr, err := internal.MakeBotMessage(req, uniqueTop10, "https://7105.api.green-api.com/waInstance7105341467/sendMessage/d4a457b0df2a45adbf8f1dd0922f9e10d97be7e200bc4dd081")
-
+		respStr, err := internal.MakeBotMessage(req, uniqueTop10, cloudflare.Getenv("GREEN_API_URL"))
 		if err != nil {
 			fmt.Println("Failed to Make Bot Message", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
 		fmt.Println(respStr)
 
 		w.Write([]byte("Msg sent to whatsapp.\n"))
 		w.Write([]byte(fmt.Sprintf("%s\n", respStr)))
-
-		if len(uniqueTop10) == 0 {
-			w.Write([]byte("No new feeds found. All top 20 stories have already been seen.\n"))
-		}
-
 	})
 	workers.Serve(nil) // use http.DefaultServeMux
 }
